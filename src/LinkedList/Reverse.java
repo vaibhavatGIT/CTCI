c882b664551b60f95456c2d088b305c4ea082bbd
vaibhav <vaibhav.eco@gmail.com>
2019-07-26 12:56:25 +0530

    Reverse inline

---
+++ src/LinkedList/Reverse.java
@@ -0,0 +1,55 @@
+class Reverse {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int key) {
+            data = key;
+            next = null;
+        }
+    }
+
+    public void push(int key) {
+        Node newNode = new Node(key);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void printNode() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data);
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public Node inplaceRev() {
+        Node prev = null, curr = head, next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Reverse r = new Reverse();
+        r.push(1);
+        r.push(2);
+        r.push(3);
+        r.push(4);
+        r.push(5);
+        r.printNode();
+        r.inplaceRev();
+        r.printNode();
+
+    }
+
+}
